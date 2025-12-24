e9e818cbd983e8a0d6f8e60f099f49150bfbd17c
SadHappy <craigjm19@gmail.com>
2025-12-24 13:55:01 +0000

    Imported my library

--- _dev/playground.py
+++ _dev/playground.py
@@ -2,11 +2,24 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+from music_theory import Note
 from music_theory_midi_tools.midi_note import MIDINote
 
 
+ 
 
-mn = MIDINote("C", "4")
+ 
 
 
-print(mn)
+
+
+
+mn = MIDINote(Note.C, 4)
+
+
+print(str(mn))
+print(repr(mn))
+print(mn.note.previous())
+
+print(mn.octave_up())
+print(mn.octave_above())
