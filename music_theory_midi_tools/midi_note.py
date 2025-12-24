6881c8afef68dd005d1961931d076aa52b5d4ae4
SadHappy <craigjm19@gmail.com>
2025-12-24 13:57:25 +0000

    Added basic functionality

--- music_theory_midi_tools/midi_note.py
+++ music_theory_midi_tools/midi_note.py
@@ -1,11 +1,29 @@
+from typing import Self
 
-class MIDINote:
-    def __init__(self, note: str, octave: int):
+from music_theory import Note
+
+class MIDINote():
+    def __init__(self, note: Note, octave: int):
         self.note = note
         self.octave = octave
 
-    def __str__(self):
+    def octave_up(self) -> Self:
+        return MIDINote(self.note, self.octave + 1)
+
+    octave_above = octave_up
+
+    def octave_down(self) -> Self:
+        return MIDINote(self.note, self.octave - 1)
+
+    octave_below = octave_up
+
+    def __str__(self) -> str:
         return f"{self.note}{self.octave}"
     
-    def __repr__(self):
-        return f"MIDINote({self.note}, {self.octave})"
+    def __repr__(self) -> str:
+        return f"MIDINote({repr(self.note)}, {self.octave})"
+    
+
+MIDDLE_C = MIDINote(Note.C, 4)
+
+
